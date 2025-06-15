d1ce248cebc3275423f6666f32fde69ee43157ec
seongbae <pseongbae@gmail.com>
2025-06-15 21:54:52 +0900

    Create modal window to save result video

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -3,6 +3,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MenuUIHandler : MonoBehaviour
 {
+    public GameObject savePopupModal;
+    void Start()
+    {
+        if (savePopupModal != null)
+        {
+            savePopupModal.SetActive(false);
+        }
+    }
     public void Move2SelectedVideoScene()
     {
         MainManager.Instance.PickVideoFromGallery();
@@ -14,8 +22,30 @@ public class MenuUIHandler : MonoBehaviour
         SceneManager.LoadScene("StartScene");
     }
 
-    public void Move2Inferecne()
+    public void Move2Inference()
     {
         SceneManager.LoadScene("InferenceScene");
     }
+
+    public void ClickDoneButton()
+    {
+        if (savePopupModal != null)
+        {
+            savePopupModal.SetActive(true);
+        }
+    }
+
+    public void MoveToStartScene(bool isSave)
+    {
+        if (isSave)
+        {
+            Debug.Log("Save the video");
+        }
+        else
+        {
+            Debug.Log("Do not save the video");
+        }
+        SceneManager.LoadScene("StartScene");
+    }
+
 }
